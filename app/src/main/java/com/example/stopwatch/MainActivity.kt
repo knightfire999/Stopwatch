6ebf81961890f389144a07c76b6633d3a2dad190
chris <liu.christopher05@gmail.com>
2020-09-25 11:18:47 -0700

    Finished base timer layout

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        chronometer_main_timer.visibility = View.INVISIBLE
+        var timeWhenStopped:Long = 0
+
+        button_main_start.setOnClickListener {
+            chronometer_main_timer.visibility = View.VISIBLE
+            button_main_start.visibility = View.INVISIBLE
+            button_main_stop.visibility = View.VISIBLE
+            button_main_reset.visibility = View.VISIBLE
+            chronometer_main_timer.base = SystemClock.elapsedRealtime() + timeWhenStopped
+            chronometer_main_timer.start()
+        }
+
+        button_main_stop.setOnClickListener {
+            timeWhenStopped = chronometer_main_timer.base-SystemClock.elapsedRealtime()
+            chronometer_main_timer.stop()
+            button_main_start.visibility = View.VISIBLE
+            button_main_reset.visibility = View.VISIBLE
+        }
+
+        button_main_reset.setOnClickListener {
+            chronometer_main_timer.base = SystemClock.elapsedRealtime()
+            timeWhenStopped = 0
+            chronometer_main_timer.stop()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause has been called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart has been called")
     }
 }
