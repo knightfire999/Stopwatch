c5ca656ae9beca11ec5d0dc77d307f8514e09023
chris <liu.christopher05@gmail.com>
2020-10-09 10:08:22 -0700

    Finished

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.os.SystemClock
 import android.util.Log
 import android.view.View
@@ -10,12 +11,22 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
+    private val KEY_STARTED = "started or not"
+    private val KEY_TIME_STOPPED = "time stopped"
+    private val KEY_TEXT = "text when stopped"
+    private val KEY_TIME_STARTED = "time started"
+    private var timeElapsed = 0L
+    private val KEY_TIME_ELAPSED = "elapsed time"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         chronometer_main_timer.visibility = View.INVISIBLE
         var timeWhenStopped:Long = 0
+
+        //?. builds an if not null
+        //?: is the elvis operator. What comes after is the default value if what comes before is null
+        timeElapsed = savedInstanceState?.getLong(KEY_TIME_ELAPSED) ?: 0L
 
         button_main_start.setOnClickListener {
             chronometer_main_timer.visibility = View.VISIBLE
@@ -40,13 +51,38 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart has been called")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart has been called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume has been called")
+    }
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause has been called")
     }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "onRestart has been called")
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop has been called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy has been called")
+    }
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        Log.d(TAG, "onSavedInstanceState has been called")
+        //in the Bundle, data is stored in key-value pairs
+        //lots of key-value pairs here: JSON = javascript object notation (very commonly used for data storage online)
+        //key: like a name tag you associate with the data
+        //value: the actual data being stored
+        outState.putLong(KEY_TIME_ELAPSED, timeElapsed)
+        timeElapsed = chronometer_main_timer.getBase() - SystemClock.elapsedRealtime()
     }
 }
